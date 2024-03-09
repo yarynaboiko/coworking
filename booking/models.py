3fd25685816e07b126489aff184dac8bd3ea1f10
Yaryna Boiko <yaryna.boiko06@gmail.com>
2024-03-09 17:13:11 +0200

    aded models

--- booking/models.py
+++ booking/models.py
@@ -15,13 +15,13 @@ class Category(models.Model):
         ordering = ['title']
 
 class Room(models.Model):
-    title = models.CharField(max_length=50)
-    capacity = models.IntegerField()
-    description = models.TextField()
+    title = models.CharField(max_length=50, verbose_name="назва кімнати")
+    capacity = models.IntegerField(verbose_name="місткість кімнати")
+    description = models.TextField(verbose_name="опис")
     image = models.ImageField(upload_to='rooms')
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    location = models.CharField(max_length=255)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name="категорія кімнати")
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="ціна кімнати")
+    location = models.CharField(max_length=255, verbose_name="локація кімнати")
 
     def __str__(self):
         return self.title
@@ -33,9 +33,9 @@ class Room(models.Model):
 
 
 class Place(models.Model):
-    title = models.CharField(max_length=50)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50, verbose_name="назва")
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="ціна")
+    room = models.ForeignKey(Room, on_delete=models.CASCADE, verbose_name="кімната")
 
     def __str__(self):
         return self.title
@@ -53,14 +53,13 @@ class Booking(models.Model):
         ("cancelled", "Скасовано бронювання"),
         ("archived", "Минуле бронювання"),
     ]
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    room = models.ForeignKey(Room, on_delete=models.CASCADE)
-    places = models.ManyToManyField(Place, related_name="bookings")
-    start_date = models.DateTimeField()
-    end_date = models.DateTimeField()
-    status = models.CharField(max_length=30, choices=BOOKING_STATUSES, default='created')
-    created_date = models.DateTimeField(auto_now_add=True)
-
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="користувач")
+    room = models.ForeignKey(Room, on_delete=models.CASCADE, verbose_name="кімната")
+    places = models.ManyToManyField(Place, related_name="bookings", verbose_name="місця")
+    start_date = models.DateTimeField(verbose_name="дата початку")
+    end_date = models.DateTimeField(verbose_name="кінцева дата")
+    status = models.CharField(max_length=30, choices=BOOKING_STATUSES, default='created', verbose_name="статус")
+    created_date = models.DateTimeField(auto_now_add=True, verbose_name="створена дата")
     def __str__(self):
         return f'{self.pk}, {self.room}'
 
